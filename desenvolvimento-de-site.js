8c97779d0a4a6dbbf820c71445f66638e3dfc5aa
Paulo Perasso <paulomonreale@gmail.com>
2025-10-02 13:01:31 -0300

    Página Serviços Tecnologia
    Utilização como base para desenvolvimento de todos as páginas de serviços

---
+++ desenvolvimento-de-site.js
@@ -0,0 +1,9 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const btn = item.querySelector(".faq-question");
+
+  btn.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
